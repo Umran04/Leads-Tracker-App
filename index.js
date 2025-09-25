89a09d5fb129b66384ada5c330c9d4d0c39d0d46
Umran <mohammedumran.0416@gmail.com>
2025-09-25 19:20:37 +0100

    refactoring chrome extension to get ready to build app around firebase

--- index.js
+++ index.js
@@ -14,19 +14,15 @@ console.log(database)
 
 const inputBtn = document.getElementById('input-btn')
 const deleteBtn = document.getElementById('delete-btn')
-const tabBtn = document.getElementById('tab-btn')
 const inputEl = document.getElementById('input-el')
 const ulEl = document.getElementById('ul-el')
 let listItems = ''
 
-let myLeads = []
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
 
-if(leadsFromLocalStorage){
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
+
+
+
 
 function render(leads){
     
@@ -53,29 +49,21 @@ inputBtn.addEventListener('click', () => {
     if(inputEl.value == ''){
         alert('enter a url')
     }
-        myLeads.push(inputEl.value)
+        console.log(inputEl.value)
         inputEl.value = ''
-        localStorage.setItem('myLeads', JSON.stringify(myLeads))
-        render(myLeads)
+        
+       
     
     
 })
 
 
 
-tabBtn.addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs =>{
-        myLeads.push(tabs[0].url)
-        localStorage.setItem('myLeads', JSON.stringify(myLeads))
-        render(myLeads)
-    });
-})
 
 
 deleteBtn.addEventListener('dblclick', () => {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    
+   
     
 })
 
